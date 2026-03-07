74c174552730c627de7949f020ca8d8bf350235f
Tushar Anand <tushar4821@gmail.com>
2026-03-07 20:17:19 +0530

    Rename footer to Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 function App() {
 
